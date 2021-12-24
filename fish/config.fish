0854ac5b68fe241506e18340b7314e6630a4377f
Saurav Khdoolia <sauravkhdoolia@gmail.com>
2021-12-24 15:33:26 +0530

    lets finally try it all

--- fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-starship init fish | source
-
-function launch
-	kitty --start-as=fullscreen --session ~/.config/kitty/mm.conf &
-end
